9eed78693f12a7e69f2f50130a6c1788b6b70ae4
Sean Cross <sean@xobs.io>
2018-12-21 13:20:19 +0800

    sim: add sim entry for setColorChain
    This is a direct copy of setColor for now...
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- sim/state/rgb.ts
+++ sim/state/rgb.ts
@@ -10,4 +10,14 @@ namespace pxsim.rgb {
         led.updateBuffer(newColor, 0);
         runtime.queueDisplayUpdate();
     }
+
+    export function setRGBLedChainSame(r: number, g: number, b: number, length: number): void {
+        const led = board().neopixelState;
+        const newColor = new Uint8Array(3);
+        newColor[0] = g; // G and R and swapped
+        newColor[1] = r;
+        newColor[2] = b;
+        led.updateBuffer(newColor, 0);
+        runtime.queueDisplayUpdate();
+    }
 }
